195d628ddc71265d71fe94523608a1a3a226b09c
Andrexus23 <andrewsamsunguser@gmail.com>
2025-06-14 17:34:42 +0300

    удаление ненужной информации

--- playground/test_gpt2.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-# Загрузить модель и токенизатор (например, GPT-2 или другую)
-generator = pipeline('text-generation', model='gpt2')
-
-prompt = "Freelancer earnings analysis shows that"
-results = generator(prompt, max_length=100, num_return_sequences=1)
-
-print(results[0]['generated_text'])
--- playground/test_ru_eng_llm.py
+++
@@ -1,7 +0,0 @@
-from xindata_income_analyzer.model.text_generator import TextGenerator
-
-if __name__ == "__main__":
-    generator = TextGenerator(model_name='/home/andrey/test-tasks/preptrained_models/updated_rugpt3small_based_on_gpt2')
-    prompt = "Насколько выше доход у фрилансеров, принимающих оплату в криптовалюте, по сравнению с другими способами оплаты?"
-    generated = generator.generate_text(prompt, max_length=150, temperature=0.8)
-    print(f"Сгенерированный текст:\n{generated}")
